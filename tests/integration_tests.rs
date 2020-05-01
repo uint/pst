e98e1e8978ccfb399de6e10733278c5b465d10fa
Tomasz Kurcz <uint@lavabit.com>
2020-05-01 19:58:39 +0200

    Remove bad test, but leave a useful snippet for future testing

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,42 +1,29 @@
-use pst::bins::{Bin, BinOwned};
-use pst::config::ConfigStore;
-use pst::backends::Backend;
-use reqwest::Client;
+// This function can be used to test pretty much any backend. I don't want to
+// test on someone else's production servers, though. Maybe in the future
+// we'll use this to test against mocks or test instances of pastebin servers.
 
-const TEST_STR: &'static str = "test string 123fd93f324";
+// use pst::bins::Bin;
+// use reqwest::Client;
 
-type BoxError = Box<dyn std::error::Error>;
+// const TEST_STR: &'static str = "test string 123fd93f324";
 
-#[test] #[ignore] // needs a better approach
-fn test_all_the_bins() -> Result<(), BoxError> {
-    let cfg_store = ConfigStore::new()?;
+// type BoxError = Box<dyn std::error::Error>;
 
-    for backend in Backend::backends_iter() {
-        let backend_name = backend.to_string();
-        let cfg = cfg_store.bin_config(&backend_name).expect("");
-        assert!(test_bin(BinOwned::new(
-            backend,
-            cfg.host,
-        )));
-    };
-    Ok(())
-}
+// fn test_bin<B: Bin>(bin: B) -> bool {
+//     let paste = bin.post(TEST_STR).unwrap();
 
-fn test_bin(bin: BinOwned) -> bool {
-    let paste = bin.post(TEST_STR).unwrap();
+//     println!("Got the API URL {:?} when testing {:?}", paste.api_url(), bin);
 
-    println!("Got the API URL {:?} when testing {:?}", paste.api_url(), bin);
+//     let client = Client::new();
 
-    let client = Client::new();
+//     let mut res = client.get(paste.api_url())
+//         .send()
+//         .expect("Couldn't post to bin");
 
-    let mut res = client.get(paste.api_url())
-        .send()
-        .expect("Couldn't post to bin");
+//     let res_text = res.text().unwrap();
 
-    let res_text = res.text().unwrap();
+//     println!("{}", res_text);
 
-    println!("{}", res_text);
-
-    // Verify we can find the string we posted if we follow the link.
-    res_text.find(TEST_STR).is_some()
-}
+//     // Verify we can find the string we posted if we follow the link.
+//     res_text.find(TEST_STR).is_some()
+// }
